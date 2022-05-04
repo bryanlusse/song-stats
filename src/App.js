1d8128dac3c40be374dba1d815cceaf500579940
Bryan Lusse <blusse7@gmail.com>
2022-05-04 15:19:51 +0800

    new structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './style/App.scss';
+import './typography/font-awesome.min.css'
+import Stats from './components/Stats/Stats';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <Stats />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
