48ebb53dc2b52ddeda8a43c548298faea6eb43ce
Bryan Lusse <blusse7@gmail.com>
2022-05-16 18:34:26 +0800

    improved dark mode, added new compare functionality and improved responsiveness

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,7 +3,9 @@ import {keepTheme, setTheme} from '../../utils/themes';
 import {RadarChart} from "./Chart";
 import {Container} from 'react-bootstrap';
 import axios from 'axios';
-import Image from "./SONGSTATS-logos_transparent.png"
+import Image from "../imgs/SONGSTATS-logos_transparent.png"
+import { ReactComponent as MoonIcon } from "../imgs/moon.svg";
+import { ReactComponent as SunIcon } from "../imgs/sun.svg";
 
 const songLabels = [
   "acousticness",
@@ -23,44 +25,61 @@ const songLabelsCap = [
   "Liveness",
   "Speechiness",
   "Valence"
-]
+];
+
+const colors = [
+  "#394648",
+  "#CFC0BD",
+  "#CBAC88",
+  "#EDB6A3",
+  "#F8E9E9"
+];
+
+const borderColors = [
+  "#242C2E",
+  "#BAA5A0",
+  "#B48855",
+  "#E49277",
+  "#EBC1C1"
+];
+
+function addAlpha(color, opacity) {
+  // coerce values so ti is between 0 and 1.
+  const _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+  return color + _opacity.toString(16).toUpperCase();
+}
 
 const Header = () => {
-  const [options, setOptions] = useState("")
   const [searchKey, setSearchKey] = useState("");
   const [gridlinecolors, setGridlinecolors] = useState(localStorage.getItem('theme') === 'theme-light' ? "rgba(0, 0, 0, 0.2)" : "rgba(255, 255, 255, 0.2)");
   const [themecolors, setThemecolors] = useState(localStorage.getItem('theme') === 'theme-light' ? "black" : "white");
   const [stats, setStats] = useState([
     {
       labels: songLabelsCap,
-      datasets: [
-        {
-          label: 'init',
-          data: [0.5,0.5,0.5,0.5,0.5,0.5,0.5],
-          backgroundColor: "#0cf0dd",
-          borderColor: "#0abdae",
-        }
-      ],
+      datasets: [{}],
     }
   ]);
-
+  const [options, setOptions] = useState("")
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const loader = document.getElementById("loader-bubbles");
+  const chart = document.getElementById("chart-container");
+  const button = document.getElementById("compare");
+  const checkbox = document.getElementById("switch");
+  const [artistId, setArtistId] = useState("");
+  const [artistName, setArtistName] = useState("");
+  const [tokenGlob, setTokenGlob] = useState("");
+  const [origSong, setOrigSong] = useState("");
 
   window.onload = function() {
-    const checkbox = document.getElementById("switch");
     const check = localStorage.getItem('theme') === 'theme-light' ? "false" : "true";
     if (check === "true") {
       checkbox.checked = 'true'
-    }
+    };
   }
 
   const getStats = async (e) => {
     e.preventDefault()
-    var loader = document.getElementById("loader-bubbles");
-    var chart = document.getElementById("chart-container");
-    var button = document.getElementById("compare");
-
     if (chart.style.display === "inline-block") {
       chart.style.display = "none";
     }
@@ -74,6 +93,7 @@ const Header = () => {
     const auth = await axios.get("https://react-spotify--app.herokuapp.com/token", {
     });
     const token = auth.data.accessToken;
+    setTokenGlob(token);
     var data = await axios.get("https://api.spotify.com/v1/search", {
         headers: {
             Authorization: `Bearer ${token}`
@@ -83,21 +103,33 @@ const Header = () => {
             type: "track"
         }
     });
-    const id = data.data.tracks.items[0].id;
 
+    const id = data.data.tracks.items[0].id;
+    setArtistId(data.data.tracks.items[0].artists[0].id);
+    setArtistName(data.data.tracks.items[0].artists[0].name);
+  
     var stat_data = await axios.get("https://api.spotify.com/v1/audio-features/" + id, {
         headers: {
             Authorization: `Bearer ${token}`
         },
     });
+
+    setOrigSong({
+      label: data.data.tracks.items[0].name + ' - ' + data.data.tracks.items[0].artists[0].name,
+      data: songLabels.map((label) => stat_data.data[label]),
+      backgroundColor: addAlpha("#0cf0dd", 0.3),
+      borderColor: addAlpha("#0abdae",0.3)
+    });
+
+
     setStats({0: {
       labels: songLabelsCap,
       datasets: [
         {
           label: data.data.tracks.items[0].name + ' - ' + data.data.tracks.items[0].artists[0].name,
           data: songLabels.map((label) => stat_data.data[label]),
-          backgroundColor: "#0cf0dd",
-          borderColor: "#0abdae"
+          backgroundColor: addAlpha("#0cf0dd", 0.3),
+          borderColor: addAlpha("#0abdae",0.3)
         }
       ]
     }});
@@ -122,7 +154,14 @@ const Header = () => {
             pointLabels: {
               color: themecolors,
               font: {
-                size: 16
+                size: function () {
+                  if (window.innerWidth < 450) {
+                    return 13
+                  }
+                  else {
+                    return 16
+                  }
+                }
               }
             },
             angleLines: {
@@ -151,7 +190,14 @@ const Header = () => {
             labels: {
               color: themecolors,
               font: {
-                size: 15
+                size: function () {
+                  if (window.innerWidth < 450) {
+                    return 10
+                  }
+                  else {
+                    return 15
+                  }
+                }
               }
             }     
           },
@@ -164,20 +210,15 @@ const Header = () => {
   }
 
   const darkMode = async (e) => {
-    console.log(localStorage.getItem('theme'))
-    console.log(themecolors)
     if (localStorage.getItem('theme') === 'theme-light') {
-      console.log('light->dark')
       setTheme('theme-dark');
       setGridlinecolors("rgba(255, 255, 255, 0.2)");
       setThemecolors("white");
     } else {
-      console.log('dark->light')
       setTheme('theme-light');
       setGridlinecolors("rgba(0, 0, 0, 0.2)");
       setThemecolors("black")
     }
-    console.log(themecolors)
   }
 
   useEffect(() => {
@@ -202,7 +243,14 @@ const Header = () => {
             pointLabels: {
               color: themecolors,
               font: {
-                size: 16
+                size: function () {
+                  if (window.innerWidth < 450) {
+                    return 13
+                  }
+                  else {
+                    return 16
+                  }
+                }
               }
             },
             angleLines: {
@@ -231,17 +279,67 @@ const Header = () => {
             labels: {
               color: themecolors,
               font: {
-                size: 15
+                size: function () {
+                  if (window.innerWidth < 450) {
+                    return 10
+                  }
+                  else {
+                    return 15
+                  }
+                }
               }
             }     
           },
         }
       }
     )
-  }, [themecolors]);
+  }, [themecolors, gridlinecolors]);
 
-  const doSomething = async (e) => {
-    
+  const compareSongs = async (e) => {
+    if (chart.style.display === "inline-block") {
+      chart.style.display = "none";
+    }
+
+    if (button.style.display === "inline-block") {
+      button.style.display = "none";
+    }
+
+    loader.style.display = "inline-block";
+
+    var data = await axios.get("https://api.spotify.com/v1/artists/" + artistId + "/top-tracks", {
+        headers: {
+            Authorization: `Bearer ${tokenGlob}`
+        },
+        params: {
+            market: "US"
+        }
+    });
+    const top5 = data.data.tracks.slice(0,5);
+    const comparisonDataset = [];
+    comparisonDataset.push(origSong); // Add original searched song first
+    var i = 0; // counter
+    for (const song of top5) {
+      var stat_data_top = await axios.get("https://api.spotify.com/v1/audio-features/" + song.id, {
+        headers: {
+            Authorization: `Bearer ${tokenGlob}`
+        },
+      });
+      var processedStats = {
+        label: song.name + ' - ' + artistName,
+        data: songLabels.map((label) => stat_data_top.data[label]),
+        backgroundColor: addAlpha(colors[i],0.3),
+        borderColor: addAlpha(borderColors[i],0.3)
+      };
+      comparisonDataset.push(processedStats);
+      i+=1;
+    };
+    setStats({0: {
+      labels: songLabelsCap,
+      datasets: comparisonDataset
+    }});
+    chart.style.display = "inline-block";
+    // button.style.display = "inline-block";
+    loader.style.display = "none";
   }
 
   useEffect(() => {
@@ -267,10 +365,14 @@ const Header = () => {
               <img className="logo" src={Image} alt="Song Stats"/>
             </a>
           </div>
-          <div className="nav-child" style={{width: "20%", height: "100%"}}>
+          <div className="nav-child" style={{width: "20%", height: "100%", marginBottom: "1%"}}>
             <label className="switch" >
               <input id="switch" type="checkbox" onClick={darkMode}/>
               <span className="slider round"></span>
+              <div className="icons">
+                <SunIcon />
+                <MoonIcon />
+              </div>
             </label>
           </div>
         </div>
@@ -305,7 +407,7 @@ const Header = () => {
           <div id="chartjs-tooltip" className="tooltiptext"></div>
         </div>
         <div>
-          <button id="compare" className="cta-btn cta-btn--stats" style={{display: "none"}} onClick={doSomething}>
+          <button id="compare" className="cta-btn cta-btn--stats" style={{display: "none"}} onClick={compareSongs}>
             Compare with other songs
           </button>
         </div>
